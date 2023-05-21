dba4c5bc74ade6e89ab3d13a6d4573a5ba3d6eb4
SalJelena <salopekjelena24@gmail.com>
2023-05-21 11:38:18 +0200

    finish error page design

--- frontend/src/pages/ErrorPage/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage/ErrorPage.jsx
@@ -1,12 +1,23 @@
-import { Link } from "react-router-dom"
+import {Link} from "react-router-dom"
+import NavBar from "../../components/NavBar/NavBar";
+import Footer from "../../components/Footer/Footer";
 
 const ErrorPage = () => {
-  return (
-    <>
-        <div>ErrorPage</div>
-        <Link to="/">Go to Home</Link>
-    </>
-  )
+    return (
+        <>
+            <NavBar/>
+            <div className="error">
+                <div className="wrap">
+                    <div className="error__inner">
+                        <h2 className="error__title">Sorry, page not found.</h2>
+                        <span className="error__number">404</span>
+                        <Link to="/" className="button button--rounded button--secondary">Go to Home Page</Link>
+                    </div>
+                </div>
+            </div>
+            <Footer/>
+        </>
+    )
 }
 
 export default ErrorPage
