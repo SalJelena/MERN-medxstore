3f823c8562e56fe818e63023843f474dd7ec043d
SalJelena <salopekjelena24@gmail.com>
2023-05-22 12:46:45 +0200

    latest changes 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import {Outlet} from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
 import axios from "axios";
+import {BASE_URL_BE} from "./config/configVars";
 
 axios.defaults.baseURL = axios.defaults.baseURL = BASE_URL_BE
 
